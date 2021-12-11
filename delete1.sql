248d31e3aea6b2ecc9504c1af0a43e47da12ec61
vyshnav2001-nitc <79520829+vyshnav2001-nitc@users.noreply.github.com>
2021-12-11 20:42:12 +0400

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE FROM books_subjects WHERE subject IN (SELECT bs.subject FROM books_subjects bs, subjects s WHERE s.name='History' AND s.id = bs.subject);
-DELETE FROM subjects WHERE name='History';
+delete from books_subject where (book,subject) in "(8,8),(8,9)";
+delete from subjects where name = "History";
